7bc7ce2c39f0be5455f550a52f328bab943ebd38
John Stachurski <john.stachurski@gmail.com>
2021-08-15 05:01:28 +1000

    misc

--- code_book/_build/jupyter_execute/ch1.py
+++
@@ -1,6 +0,0 @@
-#!/usr/bin/env python
-# coding: utf-8
-
-# # Chapter 1 Code
-# 
-# No code in this chapter.
--- code_book/_build/jupyter_execute/ch3.py
+++
@@ -1,6 +0,0 @@
-#!/usr/bin/env python
-# coding: utf-8
-
-# # Chapter 3 Code
-# 
-# No code used in this chapter.
